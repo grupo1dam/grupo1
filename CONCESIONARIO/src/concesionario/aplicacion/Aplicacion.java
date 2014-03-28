36ee359b2be61cc706d3281307d719d556cf1185
AFV <wcpx.fer@gmail.com>
2014-03-28 19:07:31 +0100

    Primer commit en a la carpeta local

---
+++ CONCESIONARIO/src/concesionario/aplicacion/Aplicacion.java
@@ -0,0 +1,13 @@
+package concesionario.aplicacion;
+
+public class Aplicacion {
+
+	public static void main(String[] args) {
+		//creo objeto de la clase
+		new Aplicacion();
+		//salida del programa
+		System.exit(0);
+
+	}
+
+}
